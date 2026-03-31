4e44f52977d3da488b1bb934205ae3f3b1e6ceb1
29xfmcfzww-afk <s600826826@gmail.com>
2026-03-31 19:38:49 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+{
+  "short_name": "富淮旗艦",
+  "name": "富淮團隊數位理財工具",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "theme_color": "#002b36",
+  "background_color": "#002b36",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/2489/2489756.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
